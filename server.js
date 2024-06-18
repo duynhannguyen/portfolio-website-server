f2160cc602495314af62194fac6d89875d2580f7
Duy Nhan <duynhannguyenn@gmail.com>
2024-06-19 03:29:10 +0700

    rollback OG vercel config

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const PORT = 3001;
 app.use(express.json());
 app.use(cors("*"));
 app.use("/api/v1", appRouter);
-app.get("/api/v1/", (req, res) => res.send("Express on Vercel"));
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
